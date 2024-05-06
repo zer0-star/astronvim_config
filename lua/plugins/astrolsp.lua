16bd9ef8566d0d12292f5139e0955b417bce123e
zer0-star <zer0star.65535@gmail.com>
2024-05-07 08:31:37 +0900

    migrate from v3

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -41,11 +39,21 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+      "clangd",
+      "rust_analyzer",
+      "hls",
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      hls = {
+        settings = {
+          haskell = {
+            formattingProvider = 'floskell',
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+  "akinsho/toggleterm.nvim",
+  keys = {
+    { "<C-h>", "<C-h>", mode = "t", noremap = true },
+    { "<C-j>", "<C-j>", mode = "t", noremap = true },
+    { "<C-k>", "<C-k>", mode = "t", noremap = true },
+    { "<C-l>", "<C-l>", mode = "t", noremap = true },
+  },
+}
