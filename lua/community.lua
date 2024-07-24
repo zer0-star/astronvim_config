7fb8578754c93fff92c6e45233409c64d5f02b8f
zer0-star <zer0star.65535@gmail.com>
2024-07-24 17:43:54 +0900

    update

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
   { import = "astrocommunity.recipes.vscode" },
 }
